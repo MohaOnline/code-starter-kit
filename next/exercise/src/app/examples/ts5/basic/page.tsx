c3cd683227ca3010d402f65cc0a24f28223f84af
njun <tweeg.ni@gmail.com>
2025-07-24 21:18:28 +0800

    demo

--- next/exercise/src/app/examples/ts5/basic/page.tsx
+++ next/exercise/src/app/examples/ts5/basic/page.tsx
@@ -10,15 +10,35 @@ import { ThemeToggle } from "@/app/lib/components/ThemeToggle";
  *
  * @returns {string}
  */
-const TypeScriptBasic: Function = (): string => {
+const demoTypeScriptBasic: () => string = (): string => {
   return "";
+};
+
+/**
+ * 返回 JSX 的可以直接当 React 组件使用
+ *
+ * @returns {React.ReactElement} JSX 元素
+ */
+const TypeScriptBasicReact: () => React.ReactElement = (): React.ReactElement => {
+  // : () => React.ReactElement 指：变量 TypeScriptBasicReact 是一个无参数并返回 React.ReactElement 类型的函数
+  // : React.ReactElement 指：箭头函数 () => {} 返回值类型为 React.ReactElement
+
+  return (
+    <>
+      <div>TypeScriptBasicReact</div>
+    </>
+  );
 };
 
 // Page 组件为页面入口，类型为 React.FC（函数式组件）
 const Page: React.FC = () => {
+  const basic = demoTypeScriptBasic();
+  console.log(basic);
+
   return (
     <>
       <ThemeToggle />
+      <TypeScriptBasicReact />
       <h1>Page</h1>
     </>
   );
