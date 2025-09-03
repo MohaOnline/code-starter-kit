77b014d77e938aadaf614c964ecf410cfacecf0b
njun <tweeg.ni@gmail.com>
2025-09-03 18:19:23 +0800

    rebase

--- next/exercise/src/lib/components/TagFields.jsx
+++ next/exercise/src/lib/components/TagFields.jsx
@@ -20,42 +20,39 @@ export function TagFieldSingle({
   const [fieldValue, setFieldValue] = React.useState(null);
 
   return (
-    <Autocomplete
-      freeSolo
-      id={_.kebabCase(label)}
-      size="small"
-      style={{width: width}}
-      options={options}
-      value={fieldValue}
-      getOptionLabel={(option) => option}
-      renderValue={(option, getItemProps) => {
-        console.log(_.kebabCase(label), 'renderValue', option);
-        return <Chip size="small" label={option} {...getItemProps()} />;
-      }}
-      renderInput={(params) => (
-        <TextField
-          {...params}
-          size="small"
-          label={label}
-          placeholder={placeholder}
-          slotProps={{
-            inputLabel: {shrink: true}, // ✅ label 始终显示
-          }}
-        />
-      )}
-      slotProps={{
-        listbox: {
-          style: {
-            maxHeight: 400, // ✅ 下拉框一次能显示更多
-          },
-        },
-      }}
-      onChange={(event, newValue) => {
-        setFieldValue(newValue);
-        if (updateHandler) {
-          updateHandler(newValue);
-        }
-      }}
+    <Autocomplete freeSolo
+                  id={_.kebabCase(label)}
+                  size="small"
+                  style={{width: width}}
+                  options={options}
+                  getOptionLabel={(option) => option}
+                  slotProps={{
+                    listbox: {
+                      style: {
+                        maxHeight: 400, // ✅ 下拉框一次能显示更多
+                      },
+                    },
+                  }}
+                  value={fieldValue}
+                  renderValue={(option, getItemProps) => {
+                    console.log(_.kebabCase(label), 'renderValue', option);
+                    return <Chip size="small" label={option} {...getItemProps()} />;
+                  }}
+                  onChange={(event, newValue) => {
+                    setFieldValue(newValue);
+                    if (updateHandler) {
+                      updateHandler(newValue);
+                    }
+                  }}
+                  renderInput={(params) => (<TextField {...params}
+                                                       size="small"
+                                                       label={label}
+                                                       placeholder={placeholder}
+                                                       slotProps={{
+                                                         inputLabel: {shrink: true}, // ✅ label 始终显示
+                                                       }}
+                    />
+                  )}
     />
   );
 }
@@ -84,7 +81,6 @@ export function TagFieldGroupSingle({
   });
 
   const [fieldValues, setFieldValues] = React.useState([]);
-
 
   return (
     <Autocomplete multiple
