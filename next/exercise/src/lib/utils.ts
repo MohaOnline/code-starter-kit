7be118779700905748824c30a1a0bc73f83caf6c
njun <tweeg.ni@gmail.com>
2025-09-20 14:38:42 +0800

    getWeight

--- next/exercise/src/lib/utils.ts
+++ next/exercise/src/lib/utils.ts
@@ -20,49 +20,67 @@ export function randomInt(min, max) {
  * 计算排序权重，或批量计算排序权重。
  */
 export function getWeight(weight1: string = '', weight2: string = '') {
-  if (!weight1 && !weight2) {
+  try {
+    if (!weight1 && !weight2) {
+      return LexoRank.middle().format();
+    } else if (!weight1) {
+      const lexoRank2 = LexoRank.parse(weight2);
+      return lexoRank2.genNext().format();
+    } else if (!weight2) {
+      const lexoRank1 = LexoRank.parse(weight1);
+      return lexoRank1.genPrev().format();
+    } else {
+      const lexoRank1 = LexoRank.parse(weight1);
+      const lexoRank2 = LexoRank.parse(weight2);
+      return lexoRank1.between(lexoRank2).format();
+    }
+  } catch (e) {
+    console.info('getWeight: ', e);
     return LexoRank.middle().format();
-  } else if (!weight1) {
-    const lexoRank2 = LexoRank.parse(weight2);
-    return lexoRank2.genNext().format();
-  } else if (!weight2) {
-    const lexoRank1 = LexoRank.parse(weight1);
-    return lexoRank1.genPrev().format();
-  } else {
-    const lexoRank1 = LexoRank.parse(weight1);
-    const lexoRank2 = LexoRank.parse(weight2);
-    return lexoRank1.between(lexoRank2).format();
+  } finally {
+
   }
 }
 export function getWeights(weight1: string = '', weight2: string = '', number: number = 3) {
   let weights = [];
-  if (!weight1 && !weight2) {
+
+  try {
+    if (!weight1 && !weight2) {
+      let weight = LexoRank.middle();
+      for (let i = 0; i < number; i++) {
+        weights.push(weight.format());
+        weight = weight.genNext();
+      }
+    } else if (!weight1) {
+      let weight = LexoRank.parse(weight2).genNext();
+      for (let i = 0; i < number; i++) {
+        weights.push(weight.format());
+        weight = weight.genNext();
+      }
+    } else if (!weight2) {
+      let weight = LexoRank.parse(weight1).genPrev();
+      for (let i = 0; i < number; i++) {
+        weights.push(weight.format());
+        weight = weight.genPrev();
+      }
+    } else {
+      for (let i = 0; i < number; i++) {
+        const weight = getWeight(weight1, weight2);
+        weights.push(weight);
+        if (i % 2 === 0) {
+          weight1 = weight;
+        } else {
+          weight2 = weight;
+        }
+      }
+    }
+  } catch (e) {
+    console.info('getWeights: ', e);
+    
     let weight = LexoRank.middle();
     for (let i = 0; i < number; i++) {
       weights.push(weight.format());
       weight = weight.genNext();
-    }
-  } else if (!weight1) {
-    let weight = LexoRank.parse(weight2).genNext();
-    for (let i = 0; i < number; i++) {
-      weights.push(weight.format());
-      weight = weight.genNext();
-    }
-  } else if (!weight2) {
-    let weight = LexoRank.parse(weight1).genPrev();
-    for (let i = 0; i < number; i++) {
-      weights.push(weight.format());
-      weight = weight.genPrev();
-    }
-  } else {
-    for (let i = 0; i < number; i++) {
-      const weight = getWeight(weight1, weight2);
-      weights.push(weight);
-      if (i % 2 === 0) {
-        weight1 = weight;
-      } else {
-        weight2 = weight;
-      }
     }
   }
 
